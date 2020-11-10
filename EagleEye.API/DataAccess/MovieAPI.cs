859291434c5d14a17d54f830c8ad762c21857072
tonyfearn@yahoo.com <tonyfearn@yahoo.com>
2020-11-10 08:26:18 +0000

    Get movie by MovieId works

--- EagleEye.API/DataAccess/MovieAPI.cs
+++ EagleEye.API/DataAccess/MovieAPI.cs
@@ -12,10 +12,8 @@ namespace EagleEye.API.DataAccess
         private const string STATSDATA_URL = @"C:\Users\User\source\repos\EagleEye.API\EagleEye.API\DataAccess\stats.csv";
         public List<Movie> GetAllMovies()
         {
-            return File.ReadAllLines(METADATA_URL)
-                .Skip(1) // skip field names
-                .Select(v => MapMovies(v))
-                .ToList();
+            var movies = ReadCsv();
+            return movies;
         }
 
         private Movie MapMovies(string row)
@@ -28,14 +26,30 @@ namespace EagleEye.API.DataAccess
             movie.Title = values[2];
             movie.Language = values[3];
             movie.Duration = values[4];
-            movie.ReleaseYear = Convert.ToDateTime(values[5]).Year;
+            movie.ReleaseYear = values[5];
 
             return movie;
         }
     
-        public Movie GetMovieById(int MovieId)
+        public Movie GetMovieById(int movieId)
         {
+            var movies = ReadCsv();
             var movie = new Movie();
+            foreach (var film in movies)
+            {
+                if(film.MovieId == movieId)
+                {
+                    if (film.Id > movie.Id)
+                    {
+                        movie.Id = film.Id;
+                        movie.MovieId = film.MovieId;
+                        movie.Title = film.Title;
+                        movie.Language = film.Language;
+                        movie.Duration = film.Duration;
+                        movie.ReleaseYear = film.ReleaseYear;
+                    }
+                }
+            }
 
             return movie;
         }
@@ -50,6 +64,14 @@ namespace EagleEye.API.DataAccess
             {
                 File.AppendAllText(fileName, row);
             }
+        }
+
+        private List<Movie> ReadCsv()
+        {
+            return File.ReadAllLines(METADATA_URL)
+                .Skip(1) // skip field names
+                .Select(v => MapMovies(v))
+                .ToList();
         }
     }
 }
